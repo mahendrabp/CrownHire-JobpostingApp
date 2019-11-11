c6c8871509caba9b953467937c11cda03850e2b4
mahendrabp <chipxitro@gmail.com>
2019-11-11 13:26:02 +0700

    add : Job Screen, Edit Job Screen, and Add Job Screen

---
+++ src/components/Jobs/Jobs.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  Image,
+  Dimensions,
+  ToastAndroid,
+  TouchableOpacity,
+  TextInput,
+  RefreshControl,
+} from 'react-native';
+import {Text, Button, Icon, Input, Spinner} from 'react-native-ui-kitten';
+import axios from 'axios';
+
+class Jobs extends Component {
+  render() {
+    return <></>;
+  }
+}
+
+export default Jobs;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -46,7 +46,7 @@ class HomeScreen extends Component {
         <StatusBar barStyle="light-content" backgroundColor="#B894FF" />
         <Drawer
           ref={ref => (this._drawer = ref)}
-          openDrawerOffset={100}
+          openDrawerOffset={200}
           tapToClose={true}
           content={<DrawerContent navigation={this.props.navigation} />}>
           <View style={styles.container}>
@@ -54,7 +54,7 @@ class HomeScreen extends Component {
               title="CROWNHIRE"
               alignment="center"
               style={styles.topNav}
-              // titleStyle={styles.titleTopNav}
+              titleStyle={styles.titleTopNav}
               leftControl={this.MenuDrawerAction()}
             />
           </View>
@@ -73,12 +73,15 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   topNav: {
-    borderRadius: 6,
+    borderRadius: 10,
     // marginTop: 12,
     shadowColor: '#000',
-    // marginHorizontal: 12,
-    elevation: 14,
-    zIndex: 2,
+    marginHorizontal: 12,
+    elevation: 14, //This adds a drop shadow to the item and affects z-order for overlapping views. , inntinya adalah menambah kayak border supaya timbul shadow (border timbul)
+    // zIndex: 2,
+  },
+  titleTopNav: {
+    fontFamily: 'Montserrat-Bold',
   },
 });
 
