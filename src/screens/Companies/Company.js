d15c20f51cde288b12bff192c2b9507528661c67
mahendrabp <chipxitro@gmail.com>
2019-11-15 05:12:31 +0700

    Add: Company List

--- src/screens/Companies/Company.js
+++ src/screens/Companies/Company.js
@@ -1,33 +1,134 @@
 import React, {Component} from 'react';
 
-import {View, StyleSheet, TouchableOpacity, Image} from 'react-native';
-import {Button} from 'react-native-ui-kitten';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TouchableHighlight,
+  Image,
+  ScrollView,
+} from 'react-native';
+import {Button, Text} from 'react-native-ui-kitten';
+import axios from 'axios';
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Left,
+  Right,
+  Body,
+} from 'native-base';
+import Modal from 'react-native-modal';
 
 import notFoundImage from '../../assets/404notfound.png';
 
 class Company extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      companies: [],
+      isVisible: false,
+    };
+  }
+
+  componentDidMount() {
+    this.getCompany();
+  }
+
+  async getCompany() {
+    const url = 'http://localhost:5200/api/v1/companies';
+    await axios
+      .get(url)
+      .then(result => {
+        const dataCompany = result.data.data;
+        // console.log(result.data.data);
+        this.setState({
+          companies: dataCompany,
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <>
-        <View style={styles.container}>
-          <View
-            style={{
-              borderRadius: 10,
-              alignItems: 'center',
-              justifyContent: 'center',
-              flexDirection: 'row',
-            }}>
-            <Image
-              style={{width: 300, height: 300, marginBottom: 20}}
-              source={notFoundImage}
-            />
-          </View>
-          <View>
-            <TouchableOpacity
-              onPress={() => this.props.navigation.navigate('HomeScreen')}>
-              <Button>Kembali</Button>
-            </TouchableOpacity>
-          </View>
+        <ScrollView>
+          <Card style={{marginVertical: 10}}>
+            {this.state.companies.map(v => (
+              <View
+                style={{
+                  marginVertical: 20,
+                  elevation: 10,
+                  borderWidth: 1,
+                  borderColor: '#fff',
+                }}>
+                <CardItem style={{justifyContent: 'center'}}>
+                  <View>
+                    <Text style={{fontFamily: 'Poppins-Regular', fontSize: 15}}>
+                      {v.name}
+                    </Text>
+                  </View>
+                </CardItem>
+                <CardItem cardBody style={{flex: 1}}>
+                  <Image
+                    source={{
+                      uri: `http://localhost:5200/public/logo/${v.logo}`,
+                    }}
+                    style={{height: 100, width: 100, flex: 1}}
+                  />
+                </CardItem>
+              </View>
+            ))}
+          </Card>
+        </ScrollView>
+        <View>
+          <Modal isVisible={this.state.isVisible}>
+            <View style={{flex: 1}}>
+              <Card>
+                <CardItem>
+                  <Left>
+                    <Thumbnail
+                      source={{
+                        uri: `http://localhost:5200/public/logo/`,
+                      }}
+                    />
+                    <Body>
+                      <Text>NativeBase</Text>
+                      <Text note>GeekyAnts</Text>
+                    </Body>
+                  </Left>
+                </CardItem>
+                <CardItem cardBody>
+                  <Image
+                    source={{
+                      uri: `http://localhost:5200/public/logo/`,
+                    }}
+                    style={{height: 200, width: null, flex: 1}}
+                  />
+                </CardItem>
+                <CardItem>
+                  <Left>
+                    <Button transparent>
+                      <Text>12 Likes</Text>
+                    </Button>
+                  </Left>
+                  <Body>
+                    <Button transparent>
+                      <Text>4 Comments</Text>
+                    </Button>
+                  </Body>
+                  <Right>
+                    <Text>11h ago</Text>
+                  </Right>
+                </CardItem>
+              </Card>
+            </View>
+          </Modal>
         </View>
       </>
     );
